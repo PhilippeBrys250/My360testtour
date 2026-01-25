e139d87d0a0c01fd4579ec0d78e8e9eed99e393c
PhilippeBrys250 <philippe.brys@proximus.be>
2026-01-25 20:17:14 +0100

    New 360 project

---
+++ data.js
@@ -0,0 +1,225 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0619",
+      "name": "DJI_0619",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4047487462268933,
+        "pitch": 0.10906549204647042,
+        "fov": 1.0058994307255211
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0832926158202323,
+          "pitch": 0.07445357418442455,
+          "rotation": 0,
+          "target": "2-dji_0637"
+        },
+        {
+          "yaw": 0.18686708252755757,
+          "pitch": 0.14844688950802087,
+          "rotation": 0,
+          "target": "3-dji_0616"
+        },
+        {
+          "yaw": -3.1379468407229503,
+          "pitch": -0.11241964402989879,
+          "rotation": 0,
+          "target": "1-dji_0620"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0958542601233363,
+          "pitch": -0.1392039028158898,
+          "title": "Mont Blanc",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-dji_0620",
+      "name": "DJI_0620",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5670941536582461,
+        "pitch": 0.18823287981606285,
+        "fov": 0.7728212560492475
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1013462272659122,
+          "pitch": 0.17556226928206264,
+          "rotation": 0,
+          "target": "0-dji_0619"
+        },
+        {
+          "yaw": -2.793118685299218,
+          "pitch": 0.14461238919338193,
+          "rotation": 0,
+          "target": "0-dji_0619"
+        },
+        {
+          "yaw": -2.181083043537731,
+          "pitch": 0.06272155734017204,
+          "rotation": 0,
+          "target": "2-dji_0637"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_0637",
+      "name": "DJI_0637",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08115172308770902,
+        "pitch": 0.12465522306538901,
+        "fov": 0.9722286804115068
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.413425559043646,
+          "pitch": 0.028694284747414756,
+          "rotation": 0,
+          "target": "1-dji_0620"
+        },
+        {
+          "yaw": 2.5141783922369854,
+          "pitch": 0.16008041137755313,
+          "rotation": 0,
+          "target": "3-dji_0616"
+        },
+        {
+          "yaw": 2.507720263761362,
+          "pitch": 0.06021747972015845,
+          "rotation": 0,
+          "target": "0-dji_0619"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dji_0616",
+      "name": "DJI_0616",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5871432827289667,
+        "pitch": 0.292800415632092,
+        "fov": 0.9508221401564655
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9659046769601378,
+          "pitch": 0.07715159729631083,
+          "rotation": 0,
+          "target": "2-dji_0637"
+        },
+        {
+          "yaw": 3.0213117387150135,
+          "pitch": 0.021190939530292496,
+          "rotation": 0,
+          "target": "1-dji_0620"
+        },
+        {
+          "yaw": 2.9807060649757275,
+          "pitch": -0.03396777298981846,
+          "rotation": 0,
+          "target": "1-dji_0620"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.752489296203807,
+          "pitch": 0.155070315718433,
+          "title": "Lac Saint-Guerin",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
