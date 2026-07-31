346b77cd985114a3e72d54c0c57504ffbbf29a82
PhilippeBrys250 <philippe.brys@proximus.be>
2026-07-31 18:40:16 +0200

    msuic

--- index.js
+++ index.js
@@ -14,6 +14,18 @@
  * limitations under the License.
  */
 'use strict';
+// Background music
+const music = document.getElementById("bgmusic");
+let musicStarted = false;
+
+function startMusic() {
+  if (musicStarted) return;
+  musicStarted = true;
+
+  music.volume = 0.3;
+  music.play().catch(err => console.log(err));
+}
+
 (function() {
   var Marzipano = window.Marzipano;
   var bowser = window.bowser;
@@ -68,6 +80,9 @@
 
   // Initialize viewer.
   var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
+
+  // Start music when the visitor first interacts with the panorama
+  panoElement.addEventListener("pointerdown", startMusic, { once: true });
   // Create scenes.
   var scenes = data.scenes.map(function(data) {
     var urlPrefix = "tiles";
